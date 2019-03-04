7788531807601e0f0bb72b8341e2c77c5a0acc6d
Incrediboxx <nickitabikow@yandex.ru>
2019-03-04 22:44:03 +0300

    Create TimeComparison.cs
    Добавлена возможность сравнивать даты

---
+++ Lab_1_ISRPO/Lab_1_ISRPO/TimeComparison.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_ISRPO
+{
+    class TimeComparison
+    {
+        //процедура для сравнения дат
+        public static int TimeCompare(DateTime date, DateTime date2)
+        {
+            if ((date.Year == date2.Year) && (date.Month == date2.Month) && (date.Day == date2.Day))
+            {
+                return 0; // даты равны
+            }
+
+            else if ((date.Year == date2.Year) && (date.Month == date2.Month) && (date.Day != date2.Day))
+            {
+                if (date.Day > date2.Day)
+                    return 1; //первая дата младше
+                else
+                    return -1; // первая дата старше
+            }
+            else if ((date.Year == date2.Year) && (date.Month != date2.Month))
+            {
+                if (date.Month > date2.Month)
+                    return 1; //первая дата младше
+                else
+                    return -1; // первая дата старше
+            }
+            else if (date.Year != date2.Year)
+            {
+                if (date.Year > date2.Year)
+                    return 1; //первая дата младше
+                else
+                    return -1; // первая дата старше
+            }
+            return 10;
+        }
+    }
+}
