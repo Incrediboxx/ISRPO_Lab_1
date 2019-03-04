495a953adb17763502fcf2220a743923c0736e9b
Incredibox <nickitabikow@yandex.ru>
2019-03-04 19:03:30 +0300

    New fucntion

--- Lab_1_ISRPO/Lab_1_ISRPO/Lab_1.cs
+++ Lab_1_ISRPO/Lab_1_ISRPO/Lab_1.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Lab_1_ISRPO {
     class Lab_1 {
+        static void PrintMessage() {
+            Console.WriteLine("HELLOWORLD!!!");
+        }
+
         static void Main(string[] args) {
             Console.WriteLine("HELLOWORLD!!!");
         }
