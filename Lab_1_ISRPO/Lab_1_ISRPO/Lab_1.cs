17b8bf0c5d15003d5a637c766bfab33363537b5b
Dobriu <alxandrdobriy@gmail.com>
2019-03-04 19:03:37 +0300

    Dobriu

--- Lab_1_ISRPO/Lab_1_ISRPO/Lab_1.cs
+++ Lab_1_ISRPO/Lab_1_ISRPO/Lab_1.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Lab_1_ISRPO {
     class Lab_1 {
+        static void Show() {
+            Console.WriteLine("HELLOWORLD!!!");
+        }
         static void Main(string[] args) {
             Console.WriteLine("HELLOWORLD!!!");
         }
