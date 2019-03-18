29a0e6de1de241327bcb1b34d662b7714d0e6217
Dobriu <alxandrdobriy@gmail.com>
2019-03-18 18:21:44 +0300

    Modified...

--- Lab_1_ISRPO/Lab_1_ISRPO/Lab_1.cs
+++ Lab_1_ISRPO/Lab_1_ISRPO/Lab_1.cs
@@ -10,22 +10,24 @@ namespace Lab_1_ISRPO {
             List<Worker> Workers = new List<Worker>();
             Filter filter = new Filter();
 
-            // Основной цикл меню
+            
             while (true) {
+                // Вывод главного меню
                 Console.WriteLine(" ---------------МЕНЮ---------------");
                 Console.WriteLine(" Добавить.........................1");
                 Console.WriteLine(" Отфильтрованный список...........2");
                 Console.WriteLine(" Исходный список..................3");
                 Console.WriteLine(" Установить фильтр................4");
                 Console.WriteLine(" Выход............................0");
-
-                // 
+                
+                // Выбор действия
                 int n = 0;
                 do {
                     Console.Write(" > ");
                 } while (!Int32.TryParse(Console.ReadLine(), out n));
 
-                switch (n) {
+                switch (n) { // Выполение выбранного действия
+
                     case 1: // Добавление элемента в список
                             Console.Clear();
                             Worker Temp = new Worker();                 
@@ -34,9 +36,8 @@ namespace Lab_1_ISRPO {
                     
                     case 2: // Вывод отфильтрованного списка
                             Console.Clear();
-                            foreach (Worker i in Workers.Where(w => filter.UseFilter(w)))
-                            {
-                                Console.WriteLine(i);
+                            foreach (Worker i in Workers) {
+                                Console.WriteLine(i.ApplyFilter(filter));
                             }
                             Console.ReadLine();
                             break;
@@ -51,72 +52,57 @@ namespace Lab_1_ISRPO {
 
                     case 4: // Установка значений фильтра
                             Console.Clear();
-                            Console.Write("ФИО: ");
-                            filter.SetName = Filter.EnterString();
-                            Console.Write("Должность: ");
-                            filter.SetPost = Filter.EnterString();
-                            Console.Write("Минимальная зарплата: ");
-                            filter.SetSalaryLower = Filter.EnterInt("Неверный формат");
-                            Console.Write("Максимальная зарплата: ");
-                            filter.SetSalaryUpper = Filter.EnterInt("Неверный формат");
-                            Console.Write("Минимальная дата: ");
-                            filter.SetBirthdayLower = Filter.EnterDateTime("Неверный формат");
-                            Console.Write("Максимальная дата: ");
-                            filter.SetBirthdayUpper = Filter.EnterDateTime("Неверный формат");
+                            filter.FiilFromConsole();
                             break;
 
                     case 0: // Выход из программы
                             return;
 
-                    default: // Значение по умолчания, для незарезервированных вариантов
-                            break;
+                    default: break;
                 }
                 Console.Clear();
             }
         }
 
+        // Cтруктура Работник
         struct Worker {
-            private String Name;        // ФИО
-            private DateTime Birthday;  // День рождения
-            private String Post;        // Должность
-            private int Salary;         // Зарплата
+            public String Name ;       // ФИО
+            public DateTime Birthday;  // День рождения
+            public String Post;        // Должность
+            public int Salary;         // Зарплата
 
             public Worker Push() {
                 // Вводим ФИО
                 Console.Write(" ФИО: ");
-                this.Name = Console.ReadLine();
+                Name = Console.ReadLine();
                 // Вводим дату рождения, проверяем вводимые данные на их корректность
                 do {
                     Console.Write(" Дата рождения (дд.ММ.гггг): ");
-                } while (!DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", null, DateTimeStyles.None, out this.Birthday));
+                } while (!DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", null, DateTimeStyles.None, out Birthday));
                 // Вводим должность
                 Console.Write(" Должность: ");
-                this.Post = Console.ReadLine();
+                Post = Console.ReadLine();
                 // Вводим зарплату, проверяем вводимые данные на их корректность
                 do {
                     Console.Write(" Зарплата: ");
-                } while (!Int32.TryParse(Console.ReadLine(), out this.Salary));
+                } while (!Int32.TryParse(Console.ReadLine(), out Salary));
                 return this;
             }
 
-            // Получаем ФИО рабочего (для сравнения)
-            public String getName() {
-                return Name;
-            }
-
-            // Получаем дату рождения рабочего (для сравнения)
-            public DateTime getBirthday() {
-                return Birthday;
-            }
-
-            // Получаем должность рабочего (для сравнения)
-            public String getPost() {
-                return Post;
-            }
-
-            // Получаем зарплату рабочего
-            public int getSalary() {
-                return Salary;
+            public Worker? ApplyFilter(Filter filter) {
+                if (filter.NameSubstr != null && !Name.Contains(filter.NameSubstr))
+                    return null;
+                if (filter.PostSubstr != null && !Post.Contains(filter.PostSubstr))
+                    return null;
+                if (filter.SalaryLower != null && Salary < filter.SalaryLower)
+                    return null;
+                if (filter.SalaryLower != null && Salary > filter.SalaryUpper)
+                    return null;
+                if (filter.BirthdayLower != null && Birthday < filter.BirthdayLower)
+                    return null;
+                if (filter.BirthdayUpper != null && Birthday > filter.BirthdayUpper)
+                    return null;
+                return this;
             }
 
             // Перегруженный метод ToString() для данной структуры
@@ -125,53 +111,29 @@ namespace Lab_1_ISRPO {
             }
         }
 
+        // Структура Фильтр
         struct Filter {
-            private string NameSubstr;          // Значение фильтра ФИО
-            private string PostSubstr;          // Значение фильтра Должность
-            private int? SalaryLower;           // Нижнее значение фильтра Зарплата
-            private int? SalaryUpper;           // Верхнее значение фильтра Зарплата
-            private DateTime? BirthdayLower;    // Нижнее значение фильтра День рождения
-            private DateTime? BirthdayUpper;    // Верхнее значение фильтра День рождения
+            public string NameSubstr;       // Значение фильтра ФИО
+            public string PostSubstr;       // Значение фильтра Должность
+            public int? SalaryLower;        // Мин значение фильтра Зарплата
+            public int? SalaryUpper;        // Макс значение фильтра Зарплата
+            public DateTime? BirthdayLower; // Мин значение фильтра День рождения
+            public DateTime? BirthdayUpper; // Макс значение фильтра День рождения
 
-            public string SetName {
-                set { NameSubstr = value; }
-            }
-
-            public string SetPost {
-                set { PostSubstr = value; }
-            }
-
-            public int? SetSalaryLower {
-                set { SalaryLower = value; }
-            }
-
-            public int? SetSalaryUpper {
-                set { SalaryUpper = value; }
-            }
-
-            public DateTime? SetBirthdayLower {
-                set { BirthdayLower = value; }
-            }
-
-            public DateTime? SetBirthdayUpper {
-                set { BirthdayUpper = value; }
-            }
-
-            // Сравнение с фильтром, true - подходит, false - не подходит
-            public bool UseFilter(Worker worker) {
-                if ((NameSubstr != null) && (!worker.getName().Contains(NameSubstr)))
-                    return false;
-                if ((PostSubstr != null) && (!worker.getPost().Contains(PostSubstr)))
-                    return false;
-                if ((SalaryLower != null) && (worker.getSalary() < SalaryLower))
-                    return false;
-                if ((SalaryLower != null) && (worker.getSalary() > SalaryUpper))
-                    return false;
-                if ((BirthdayLower != null) && (worker.getBirthday() < BirthdayLower))
-                    return false;
-                if ((BirthdayUpper != null) && (worker.getBirthday() > BirthdayUpper))
-                    return false;
-                return true;
+            // Функция установки значений фильтра с консоли
+            public void FiilFromConsole() {
+                Console.Write("ФИО: ");
+                NameSubstr = EnterString();
+                Console.Write("Должность: ");
+                PostSubstr = EnterString();
+                Console.Write("Минимальная зарплата: ");
+                SalaryLower = EnterInt("Неверный формат");
+                Console.Write("Максимальная зарплата: ");
+                SalaryUpper = EnterInt("Неверный формат");
+                Console.Write("Минимальная дата: ");
+                BirthdayLower = EnterDateTime("Неверный формат");
+                Console.Write("Максимальная дата: ");
+                BirthdayUpper = EnterDateTime("Неверный формат");
             }
 
             // Функция которая при чтении пустой строки вернет null
@@ -205,7 +167,8 @@ namespace Lab_1_ISRPO {
                         string value = Console.ReadLine();
                         if (value != string.Empty)
                             return DateTime.Parse(value);
-                        else return null;
+                        else
+                            return null;
                     }
                     catch (FormatException) {
                         Console.WriteLine(reEnterText);
